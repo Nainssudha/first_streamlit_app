e7deedf3110e934321a9a13d230cf33e72fd489e
Nainthara <110654869+Nainssudha@users.noreply.github.com>
2022-08-05 13:29:31 +0530

    Update streamlit_app.py
    Changed header and added 3 items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega3 and blueberry oatmeal')
-streamlit.text('Kale spinach and Rocket Smoothie')
-streamlit.text('Hard-boiled free-range Egg')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🍛Omega3 and blueberry oatmeal')
+streamlit.text('🥗🍦Kale spinach and Rocket Smoothie')
+streamlit.text('🍳Hard-boiled free-range Egg')
