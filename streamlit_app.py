fca834067c8f7346b542e63039f240038d180e43
Nainthara <110654869+Nainssudha@users.noreply.github.com>
2022-08-12 19:28:05 +0530

    Update streamlit_app.py
    To take the json version of the response and normalise it and output it to the screen in a tabular form

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,4 +20,8 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+# take the json version of the response and normalise it
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# output it to the screen in a tabular form
+streamlit.dataframe(fruityvice_normalized)
 
