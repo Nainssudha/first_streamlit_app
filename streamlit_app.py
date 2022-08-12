3e7b39e2f0fb2d84865b483e30000d965f32994e
Nainthara <110654869+Nainssudha@users.noreply.github.com>
2022-08-12 19:23:33 +0530

    Update streamlit_app.py
    Add new section to display fruityvice api response

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit
 import pandas
+import requests
+
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Favorites')
@@ -14,3 +16,7 @@ Fruits_selected=streamlit.multiselect("Pick some fruits:",list(my_fruit_list.ind
 fruits_to_show=my_fruit_list.loc[Fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
+#New section to display fruityvice api response
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
+
